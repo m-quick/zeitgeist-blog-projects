1be11aaace0a3a255d94b16ced024784c75db860
Martin Quick <martinquick7@gmail.com>
2024-07-21 17:00:34 +0100

    add function to calculate weighted gini coefficient

---
+++ political_fragmentation/utils.py
@@ -0,0 +1,12 @@
+from typing import Optional
+import numpy as np
+
+
+def gini(x: list, weights: Optional[list] = None):
+    if weights is None:
+        weights = np.ones_like(x)
+    weights_relative = [w / sum(weights) for w in weights]
+    count = np.multiply.outer(weights_relative, weights_relative)
+    mad = np.abs(np.subtract.outer(x, x) * count).sum() / count.sum()
+    rmad = mad / np.average(x, weights=weights_relative)
+    return 0.5 * rmad
