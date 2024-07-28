109d537cb77b3ecf70d20b3c0f95e940969c5b43
Martin Quick <martinquick7@gmail.com>
2024-07-28 18:08:39 +0100

    return 0 if candidate has no votes at all so perfectly even gini coeff

--- political_fragmentation/utils.py
+++ political_fragmentation/utils.py
@@ -5,6 +5,8 @@ import numpy as np
 def gini(x: list, weights: Optional[list] = None):
     if weights is None:
         weights = np.ones_like(x)
+    if sum(weights) == 0:
+        return 0
     weights_relative = [w / sum(weights) for w in weights]
     count = np.multiply.outer(weights_relative, weights_relative)
     mad = np.abs(np.subtract.outer(x, x) * count).sum() / count.sum()
